acb377df46a9d0fe5d1d8f8c494d6118881fac5b
Lordsheep13 <kinggoat343@gmail.com>
2025-08-22 20:52:54 +0100

    added calc button

--- pkTypeCalculator.c
+++ pkTypeCalculator.c
@@ -3,10 +3,19 @@
 
 static void activate(GtkApplication *app, gpointer user_data){
 	GtkWidget *window;
+	GtkWidget *grid;
+	GtkWidget *calcbtn;
 
 	window = gtk_application_window_new(app);
 	gtk_window_set_title(GTK_WINDOW(window),"Window");
 	gtk_window_set_default_size(GTK_WINDOW(window),200,200);
+	
+	grid = gtk_grid_new();
+	gtk_container_add(GTK_CONTAINER(window),grid);
+
+	calcbtn = gtk_button_new_with_label("calculate");
+	gtk_grid_attach(GTK_GRID(grid),calcbtn,0,0,2,1);
+
 	gtk_widget_show_all(window);
 }
 
@@ -15,7 +24,7 @@ int main(int argc,char **argv){
 	GtkApplication *app;
 	int status;
 
-	app = gtk_application_new("org.gtk.example",G_APPLICATION_DEFAULT_FLAGS);
+	app = gtk_application_new("pokemon.type.calculator",G_APPLICATION_DEFAULT_FLAGS);
 	g_signal_connect(app,"activate",G_CALLBACK(activate),NULL);
 	status = g_application_run(G_APPLICATION(app),argc,argv);
 	g_object_unref(app);
