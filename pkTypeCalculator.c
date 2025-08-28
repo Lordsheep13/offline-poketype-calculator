e4d805dd0dd5d166b80ddf9a486788b9677c03ec
Lordsheep13 <kinggoat343@gmail.com>
2025-08-28 03:56:43 +0100

    began steps to gui v1

--- pkTypeCalculator.c
+++ pkTypeCalculator.c
@@ -3,18 +3,29 @@
 
 static void activate(GtkApplication *app, gpointer user_data){
 	GtkWidget *window;
-	GtkWidget *grid;
-	GtkWidget *calcbtn;
+	GtkWidget *stack_switcher;
+	GtkWidget *side_stack_atk;
+	GtkWidget *side_stack_def;
+	GtkWidget *atkbtn;
+	GtkWidget *defbtn;
 
 	window = gtk_application_window_new(app);
 	gtk_window_set_title(GTK_WINDOW(window),"Window");
 	gtk_window_set_default_size(GTK_WINDOW(window),200,200);
 	
-	grid = gtk_grid_new();
-	gtk_container_add(GTK_CONTAINER(window),grid);
 
-	calcbtn = gtk_button_new_with_label("calculate");
-	gtk_grid_attach(GTK_GRID(grid),calcbtn,0,0,2,1);
+	stack_switcher = gtk_stack_switcher_new();
+	gtk_container_add(GTK_CONTAINER(window),stack_switcher);
+	side_stack_atk = gtk_stack_new();
+	side_stack_def = gtk_stack_new();
+
+	gtk_stack_switcher_set_stack(GTK_STACK_SWITCHER(stack_switcher),GTK_STACK(side_stack_atk));
+	gtk_stack_switcher_set_stack(GTK_STACK_SWITCHER(stack_switcher),GTK_STACK(side_stack_def));
+
+	atkbtn = gtk_button_new_with_label("atk");
+	defbtn = gtk_button_new_with_label("def");
+	gtk_stack_add_titled(GTK_STACK(side_stack_atk),atkbtn,"button","page2");
+	gtk_stack_add_titled(GTK_STACK(side_stack_def),defbtn,"button","page1");
 
 	gtk_widget_show_all(window);
 }
